7992fda3a4fdc5e872971accdf486a7230f2d803
eddiejmun <36680093+eddiejmun@users.noreply.github.com>
2021-03-27 15:32:45 -0700

    Update Home.tsx

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -85,7 +85,7 @@ const Home: React.FC = () => {
         <Heading as="h1" size="xl" mb="24px" color="secondary">
           {TranslateString(576, 'TitanSwap')}
         </Heading>
-        <Text>{TranslateString(578, 'The best AMM and yield farm on Binance Smart Chain.')}</Text>
+        <Text>{TranslateString(578, 'The best AMM and yield farm on Binance Smart Chain')}</Text>
       </Hero>
       <div>
         <Cards>
